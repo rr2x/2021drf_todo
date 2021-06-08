c243dcdfd98a63a10ea9619cd2c7572fb176f455
rr2x <79620740+rr2x@users.noreply.github.com>
2021-06-08 16:03:13 +0800

    frontend react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
 function App() {
+  const[items, setItems] = useState(undefined);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('http://127.0.0.1:8000/api/', { method: 'GET' });
+      const lists = await response.json();
+      setItems(lists);
+    })()
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        (items === undefined
+          ? null
+          : items.map(item=>(
+              <div key={item.id}>
+                <h1>{item.title}</h1>
+                <p>{item.body}</p>
+              </div>
+        )))
+      }
     </div>
   );
 }
